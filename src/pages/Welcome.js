6fa43007c968468ba297c18ae260a33c934eb2ea
Henrique-mazzoleni <henrique.mazzoleni@gmail.com>
2022-05-14 11:22:05 +0200

    nested Route

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,3 +1,17 @@
-const Welcome = () => <h1>Welcome Page</h1>  
+import { Fragment } from 'react';
+
+import { Route } from 'react-router-dom';
+
+const Welcome = () => {
+    return (
+        <Fragment>
+            <h1>Welcome Page</h1>  
+            <Route path="/welcome/user">
+                <h2>Nice to see you again!</h2>
+            </Route>
+        </Fragment>
+
+    )
+}
 
 export default Welcome;
